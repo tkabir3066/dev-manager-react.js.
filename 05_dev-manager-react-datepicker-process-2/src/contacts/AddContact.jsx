525e7f3e55cc1b5eb0095794a16b9aecdd52c4e9
Tutul Kabir <tutulkabir@gmail.com>
2024-08-17 15:43:27 +0530

    react date picker process 2

---
+++ 05_dev-manager-react-datepicker-process-2/src/contacts/AddContact.jsx
@@ -0,0 +1,237 @@
+import React, { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+function AddContact({ addContact }) {
+  const [contact, setContact] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    profession: "",
+    gender: "male",
+    image: "",
+    bio: "",
+  });
+
+  const [startDate, setStartDate] = useState(new Date());
+  const { firstName, lastName, email, profession, gender, image, bio } =
+    contact;
+
+  const handleChange = (e) => {
+    setContact({
+      ...contact,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(contact);
+    console.log("Submitted Successfully");
+    // checking validation
+
+    //form submission
+    addContact(contact);
+    setContact({
+      firstName: "",
+      lastName: "",
+      email: "",
+      profession: "",
+      gender: "male",
+      dateOfBirth: new Date(),
+      image: "",
+      bio: "",
+    });
+  };
+  return (
+    <>
+      <h2 className="text-center">Add Contact</h2>
+      <Form onSubmit={handleSubmit}>
+        {/* firstName field */}
+        <Form.Group as={Row} className="mb-3">
+          <Col xs={3}>
+            <Form.Label htmlFor="firstName" column>
+              First Name
+            </Form.Label>
+          </Col>
+          <Col xs={9}>
+            <Form.Control
+              type="text"
+              name="firstName"
+              id="firstName"
+              onChange={handleChange}
+              value={firstName}
+              placeholder="Enter First Name"
+            />
+          </Col>
+        </Form.Group>
+        {/* lastName field */}
+        <Form.Group as={Row} className="mb-3">
+          <Col xs={3}>
+            <Form.Label htmlFor="lastName" column>
+              Last Name
+            </Form.Label>
+          </Col>
+          <Col xs={9}>
+            <Form.Control
+              type="text"
+              name="lastName"
+              id="filaName"
+              onChange={handleChange}
+              value={lastName}
+              placeholder="Enter Last Name"
+            />
+          </Col>
+        </Form.Group>
+        {/* email field */}
+        <Form.Group as={Row} className="mb-3">
+          <Col xs={3}>
+            <Form.Label htmlFor="email" column>
+              Email Id
+            </Form.Label>
+          </Col>
+          <Col xs={9}>
+            <Form.Control
+              type="email"
+              name="email"
+              id="email"
+              onChange={handleChange}
+              value={email}
+              placeholder="Enter Your Email Id"
+            />
+          </Col>
+        </Form.Group>
+        {/* profession field */}
+        <Form.Group as={Row} className="mb-3">
+          <Col xs={3}>
+            <Form.Label htmlFor="profession" column>
+              Profession
+            </Form.Label>
+          </Col>
+          <Col xs={9}>
+            <Form.Control
+              type="text"
+              name="profession"
+              id="profession"
+              onChange={handleChange}
+              value={profession}
+              placeholder="Enter Your Profession"
+            />
+          </Col>
+        </Form.Group>
+        {/* image field */}
+        <Form.Group as={Row} className="mb-3">
+          <Col xs={3}>
+            <Form.Label htmlFor="image" column>
+              Profile Picture
+            </Form.Label>
+          </Col>
+          <Col xs={9}>
+            <Form.Control
+              type="text"
+              name="image"
+              id="image"
+              onChange={handleChange}
+              value={image}
+              placeholder="Enter Your Profile Picture URL"
+            />
+          </Col>
+        </Form.Group>
+
+        {/* dateOfBirth field */}
+        <Form.Group as={Row} className="mb-3">
+          <Col xs={3}>
+            <Form.Label htmlFor="dateOfBirth" column>
+              Email Id
+            </Form.Label>
+          </Col>
+          <Col xs={9}>
+            {/* <Form.Control
+              type="date"
+              name="dateOfBirth"
+              id="dateOfBirth"
+              onChange={handleChange}
+              value={dateOfBirth}
+              placeholder="Enter Your Date of Birth"
+            /> */}
+
+            <DatePicker
+              selected={startDate}
+              name="dateOfBirth"
+              id="dateOfBirth"
+              onChange={(date) => setStartDate(date)}
+              maxDate={new Date()}
+              // peekNextMonth
+              showMonthDropdown
+              showYearDropdown
+              dropdownMode="select"
+            />
+          </Col>
+        </Form.Group>
+
+        {/* gender field */}
+        <Form.Group as={Row} className="mb-3">
+          <Col xs={3}>
+            <Form.Label htmlFor="gender" column>
+              Gender
+            </Form.Label>
+          </Col>
+          <Col xs="auto">
+            <Form.Check
+              type="radio"
+              label="Male"
+              name="gender"
+              value="male"
+              checked={gender === "male"}
+              onChange={handleChange}
+            />
+          </Col>
+          <Col xs="auto">
+            <Form.Check
+              type="radio"
+              label="Female"
+              name="gender"
+              value="Female"
+              checked={gender === "Female"}
+              onChange={handleChange}
+            />
+          </Col>
+          <Col xs="auto">
+            <Form.Check
+              type="radio"
+              label="Other"
+              name="gender"
+              value="other"
+              checked={gender === "other"}
+              onChange={handleChange}
+            />
+          </Col>
+        </Form.Group>
+
+        {/* Bio field */}
+        <Form.Group as={Row} className="mb-3">
+          <Col xs={3}>
+            <Form.Label htmlFor="bio" column>
+              Bio
+            </Form.Label>
+          </Col>
+          <Col xs={9}>
+            <Form.Control
+              as="textarea"
+              type="text"
+              name="bio"
+              id="bio"
+              onChange={handleChange}
+              value={bio}
+              placeholder="Enter Your Bio"
+            />
+          </Col>
+        </Form.Group>
+
+        <Button variant="primary" size="md" type="submit">
+          Add Contact
+        </Button>
+      </Form>
+    </>
+  );
+}
+
+export default AddContact;
---
+++ 05_dev-manager-react-datepicker-process-2/src/layouts/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+
+function Header() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#" className="brand">
+          Dev Manager
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Contacts</Nav.Link>
+            <Nav.Link href="#action2">Register</Nav.Link>
+            <Nav.Link href="#" disabled>
+              Login
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
