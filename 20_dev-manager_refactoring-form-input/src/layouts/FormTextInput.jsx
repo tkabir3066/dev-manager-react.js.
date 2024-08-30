b3860ea9840f8fe100bb16628625c6e6c886defa
Tutul Kabir <tutulkabir@gmail.com>
2024-08-30 20:27:52 +0530

    refactoring form input

---
+++ 20_dev-manager_refactoring-form-input/src/layouts/FormTextInput.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Form, Row, Col } from "react-bootstrap";
+function FormTextInput({
+  name,
+  label,
+  placeholder,
+  type = "text",
+  errors,
+  register,
+  defaultValue,
+  ...rest
+}) {
+  return (
+    <Form.Group as={Row} className="mb-3">
+      <Col xs={3}>
+        <Form.Label htmlFor={name} column>
+          {label}
+        </Form.Label>
+      </Col>
+      <Col xs={9}>
+        <Form.Control
+          type={type}
+          id={name}
+          defaultValue={defaultValue}
+          {...register(name)}
+          isInvalid={errors?.name}
+          placeholder={placeholder}
+          {...rest}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors[name]?.message}
+        </Form.Control.Feedback>
+      </Col>
+    </Form.Group>
+  );
+}
+
+export default FormTextInput;
---
+++ 20_dev-manager_refactoring-form-input/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "react-toastify/dist/ReactToastify.min.css";
+import { ContactProvider } from "./context/Contact.context.jsx";
+//== Dev Manager ==//
+//1.CRUD
+//2.Form Handling
+//3.Remote API server connection and Handling
+//4.Routing
+//5.Context API
+//6.Authentication(Registration, Login, Logout)
+//7.Advance login - forgot password, password reset, email sending
+//8.Image upload, pagination
+//9.Search functionality
+//10.Securely Identify the user(authorization)
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ContactProvider>
+      <App />
+    </ContactProvider>
+  </StrictMode>
+);
