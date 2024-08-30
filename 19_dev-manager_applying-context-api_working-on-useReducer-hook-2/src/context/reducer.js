0f37cfba6f25dea7ac40caa0024ce7352a3183c7
Tutul Kabir <tutulkabir@gmail.com>
2024-08-30 18:39:53 +0530

    separated action's types and reducer funtion to another files

---
+++ 19_dev-manager_applying-context-api_working-on-useReducer-hook-2/src/context/reducer.js
@@ -0,0 +1,40 @@
+import { ADD_CONTACT, DELETE_CONTACT, UPDATE_CONTACT } from "./types";
+import { v4 as uuidv4 } from "uuid";
+
+export const contactsReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    //delete contact
+    case DELETE_CONTACT:
+      const updatedContacts = state.filter((contact) => contact.id !== payload);
+      return [...updatedContacts];
+
+    //update contact
+    case UPDATE_CONTACT:
+      const { contactToUpdate, id } = payload;
+      const contacts = state.map((contact) => {
+        if (contact.id === id) {
+          return {
+            id: id,
+            ...contactToUpdate,
+          };
+        } else {
+          return contact;
+        }
+      });
+
+      return [...contacts];
+
+    //add contact
+    case ADD_CONTACT:
+      const contactToAdd = {
+        id: uuidv4(),
+        ...payload,
+      };
+      return [contactToAdd, ...state];
+
+    default:
+      return state;
+  }
+};
