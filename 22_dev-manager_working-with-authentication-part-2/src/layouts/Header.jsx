d3645142379bdef7221f13d8e610452eb97d314f
Tutul Kabir <tutulkabir@gmail.com>
2024-08-31 17:14:07 +0530

    authentication part 2 login and logout

---
+++ 22_dev-manager_working-with-authentication-part-2/src/layouts/Header.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from "react";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../context/Auth.Context";
+function Header() {
+  const { logout, user } = useContext(AuthContext);
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/" className="brand">
+          Dev Manager
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            {user && (
+              <>
+                <Nav.Link as={NavLink} to="/contacts">
+                  Contacts
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/add-contact">
+                  Add Contact
+                </Nav.Link>
+                <Nav.Link onClick={logout}>Logout</Nav.Link>
+              </>
+            )}
+
+            {!user && (
+              <>
+                <Nav.Link as={NavLink} to="/register">
+                  Register
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/login">
+                  Login
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
